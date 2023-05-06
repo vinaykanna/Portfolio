5f45a136f4c53b722ebcc207ca150b91fe6cb21d
vinay <vinay@janaspandana.in>
2023-05-06 19:03:26 +0530

    change font size

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-8xl">Portfolio Website</h1>
+      <h1 className="text-5xl">Portfolio Website</h1>
     </main>
   );
 }
