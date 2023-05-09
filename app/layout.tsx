0d80e74b92397fb73ebb730b02d9dd34b8cf7393
vinay <vinay@janaspandana.in>
2023-05-09 14:00:21 +0530

    ui design and development

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Portfolio",
+  title: "Vinay Kumar",
   description: "My personal portfolio",
 };
 
---
+++ utils/data.ts
@@ -0,0 +1,99 @@
+export const technstack = [
+  {
+    name: "HTML",
+    image: "/html-5.png",
+  },
+  {
+    name: "CSS",
+    image: "/css-3.png",
+  },
+  {
+    name: "JavaScript",
+    image: "/js.png",
+  },
+  {
+    name: "TypeScript",
+    image: "/typescript.png",
+  },
+  {
+    name: "React",
+    image: "/react.svg",
+  },
+  {
+    name: "NextJS",
+    image: "/nextjs.svg",
+  },
+  {
+    name: "Svelte",
+    image: "/svelte.png",
+    width: 150,
+    height: 150,
+  },
+  {
+    name: "SolidJS",
+    image: "/solid.svg",
+    width: 150,
+    height: 150,
+  },
+  {
+    name: "NodeJS",
+    image: "/nodejs.svg",
+  },
+  {
+    name: "MySQL",
+    image: "/mysql.svg",
+  },
+  {
+    name: "MangoDB",
+    image: "/mongodb.svg",
+    width: 150,
+    height: 150,
+  },
+  {
+    name: "AWS",
+    image: "/aws.png",
+  },
+];
+
+export const projects = [
+  {
+    name: "Cloud Storage",
+    image: "/cloudstorage.png",
+    description:
+      "It is a cloud storage application where you can upload files and share them with others. It is built using ReactJS, NodeJS, ExpressJS, MySQL, and AWS S3.",
+    link: "https://cloud-storage.technologyexplorer.dev",
+    source: "https://github.com/vinaykanna/cloud-storage",
+  },
+  {
+    name: "Form Builder",
+    image: "/formbuilder.png",
+    description:
+      "It is a form builder application where you can create forms and share them with others. It is built using ReactJS, NodeJS, ExpressJS, MongoDB, and AWS S3.",
+    link: "https://form-builder.technologyexplorer.dev",
+    source: "https://github.com/vinaykanna/form-builder",
+  },
+];
+
+export const guiChallenges = [
+  {
+    name: "Auto Scroll To Bottom",
+    description:
+      "This is a GUI challenge where I have to create a chat application where the chat should auto scroll to bottom when a new message is added.",
+    link: "https://gui-challenges.technologyexplorer.dev",
+    source: "https://github.com/vinaykanna/gui-challenges",
+  },
+  {
+    name: "Infinite Scroll",
+    description:
+      "This is a GUI challenge where I have to create a page where the content should load when the user scrolls to the bottom of the page.",
+    link: "https://gui-challenges.technologyexplorer.dev",
+    source: "https://github.com/vinaykanna/gui-challenges",
+  },
+  {
+    name: "Multilevel Menu",
+    description:
+      "This is a GUI challenge where I have to create a multilevel menu where the menu should open when the user clicks on the menu item.",
+    link: "https://gui-challenges.technologyexplorer.dev",
+    source: "https://github.com/vinaykanna/gui-challenges",
+  },
+];
